e8c96eef6be5de59159bb092f0f0445817cb191a
Mazharaliruk <97372288+Mazharaliruk@users.noreply.github.com>
2022-08-05 10:09:33 +0500

    init commit

--- lib/pages/homepage/single_product_widget.dart
+++ lib/pages/homepage/single_product_widget.dart
@@ -2,10 +2,109 @@ import 'package:flutter/material.dart';
 
 
 class SingleProductWidget extends StatelessWidget {
-  const SingleProductWidget({Key? key}) : super(key: key);
+  String url;
+  String name;
+  double price;
+  double grams;
+   SingleProductWidget({Key? key,required this.url, required this.name, required this.price, required this.grams}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: [
+        Container(
+          height: 220,
+          width: 180,
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  flex: 2,
+                  child: Image.network(url)
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      name,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    Text(
+                      "$price \$ / $grams Gram",
+                      style: TextStyle(
+                          color: Colors.grey
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    flex: 2,
+                                    child: Text(
+                                      "$grams Gram",
+                                      style: TextStyle(
+                                          color: Colors.grey,
+                                          fontSize: 12
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Icon(Icons.arrow_drop_down),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 3,),
+                        Expanded(
+                          child: Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                                border: Border.all(),
+                                borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(Icons.remove,color: Colors.amber,),
+                                Text("1",style: TextStyle(color: Colors.amber),),
+                                Icon(Icons.add,color: Colors.amber,)
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
