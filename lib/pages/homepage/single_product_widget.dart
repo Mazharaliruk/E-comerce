83a1a18a737cfedc35f74c84ebaf5549e775e3b9
Mazharaliruk <97372288+Mazharaliruk@users.noreply.github.com>
2022-08-05 09:58:44 +0500

    init commit

---
+++ lib/pages/homepage/single_product_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SingleProductWidget extends StatelessWidget {
+  const SingleProductWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
